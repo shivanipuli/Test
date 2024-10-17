82893d0bf053f1443f817cfc3886b94ef65ac45e
Shivani <41695241+shivanipuli@users.noreply.github.com>
2024-10-17 17:47:14 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+def fibonacci(p):
+    fibVals = [0, 1, 1]
+        int n = 3
+        while n <= p:
+            fibVals[n] = fibVals[n-1] + fibVals[n-2]
+        return fibVals[p]
+
 def fib(p):
     fibVals = [0, 1, 1]
     int n = 3
